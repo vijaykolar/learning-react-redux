d36f1f7962b6db1b5ef4322364a6583047d06803
vijaykolar <vijayikolar@gmail.com>
2019-03-15 10:15:02 +0530

    Code cleanup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        works fd dsf
+      </div>
+    );
+  }
+}
+
+export default App;
